56c857a3a753ea2d74e87d16792833d577dbf913
Lin Fangyuan <linfangyuan99@gmail.com>
2020-06-20 12:56:52 +0000

    Added Bootstrap JavaScript requirements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// Asset pipeline - This manifest file will take all the files in the javascript/channels directory (in alphabetical order) &
+// inject them into this file, right where it says "//= require tree". This is to increase the website's speed &
+// reduce server costs, as in the real world website traffic can be very high at any given time.
+
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
