cbde4d8611f223c78c4e02bfcaf3053c3d7b3435
Lin Fangyuan <linfangyuan99@gmail.com>
2020-06-29 09:01:14 +0000

    Added separate signup pages for each plan, added CC info fields for billing, added Stripe gem for payment processing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
-    def home
-        @basicPlan = Plan.find(1)
-        @proPlan = Plan.find(2)
-    end
-    
-    def about
-    end
+  def home
+    @basicPlan = Plan.find(1)
+    @proPlan = Plan.find(2)
+  end
+  
+  def about
+  end
 end
